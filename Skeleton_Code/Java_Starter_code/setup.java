fa27ac61630b0ce08ae60fe9a2cdfd672efcb527
Charlie Fechtig <fechtigcharlie@gmail.com>
2022-04-24 11:19:19 -0400

    first commit (with nikita's code)

---
+++ Skeleton_Code/Java_Starter_code/setup.java
@@ -0,0 +1,81 @@
+import java.io.File;
+
+/*
+
+  // Constraints:
+  // name -> alhpanumeric characters including underscores and periods
+      for adding student: if name already exists: error occurs
+      for deleting student: if name does not exist: error occurs 
+  // key -> string of hex digits
+  // assignment name -> alphabet (upper and lower), 0-9, no spaces
+      for add assignment: if same name is already existing then an error occurs
+      for delete assignment: if name does not exist then error occurs
+  // points -> >=0
+  // weight -> real number in [0,1] (if new weight + current weights > 1 then error)
+  // grade -> >=0
+      for adding grade: if student name OR assignment doesnt exist: error occurs
+      if grade for this assignment is already there, then replace it 
+
+
+  class Assignment {
+    public String name;
+    public Integer points;
+    public float weight;
+  };
+
+  class Student {
+    public String first;
+    public String last;
+  }
+
+  class Grade {
+    Student student;
+    Assignment assignment;
+    Int grade;
+  }
+
+  class GradeBook {
+    String course;
+    Set<Assignmment> assignments;
+    Set<Student> students;
+    Set<Grade> grades;
+  };
+  */
+/**
+ * Initialize gradebook with specified name and generate a key.
+ */
+public class setup {
+
+  /* test whether the file exists */
+  private static boolean file_test(String filename) {
+    File tempfile = new File(filename);
+    if (tempfile.exists() == true) {
+      System.out.print("invalid");
+      System.exit(-1);
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    String key = "hello";
+
+    if (args.length < 2) {
+      System.out.println("Usage: setup <logfile pathname>");
+      System.exit(1);
+    }
+
+    if (!args[0].equals("-N")) {
+      System.exit(1);
+    }
+    String filename = args[1];
+    file_test(filename);
+
+    Gradebook gb = new Gradebook();
+    gb.writeToFile(filename);
+  /* add your code here */
+
+    System.out.println("Key is: " + key);
+
+    return;
+  }
+}
